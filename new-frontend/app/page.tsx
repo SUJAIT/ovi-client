6656070e97f7e630146f328d90e55129d8eb72ee
SUJAIT <mdsujaitullah@gmail.com>
2026-03-28 19:53:46 +0600

    prodcution Added

--- new-frontend/app/page.tsx
+++ new-frontend/app/page.tsx
@@ -1,5 +1,21 @@
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
+
+// export default function Home() {
+//   redirect("/dashboard")
+// }
+
+
+"use client"
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-  redirect("/dashboard")
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push("/dashboard")
+  }, [])
+
+  return null
 }
